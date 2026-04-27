da5d065852e8715d816a2a87afe2927f92a072f1
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2026-04-27 17:43:53 +0800

    test(metrics): percentile min

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,3 +5,7 @@ from speechloom import metrics as m
 
 def test_percentile_median():
     assert m.percentile([0, 10], 0.5) == 5
+
+
+def test_percentile_min():
+    assert m.percentile([2, 1], 0) == 1
